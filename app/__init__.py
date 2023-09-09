8c00b05a25f572363bab2b33f922dcf33d78e155
Andrew Subotin <subotinandrey5@gmail.com>
2023-09-09 12:33:27 +0200

    Register app in SQLAlchemy and Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
 
 from config import config
@@ -10,10 +11,18 @@ class Base(DeclarativeBase):
     pass
 
 
+db = SQLAlchemy(metadata=Base.metadata)
+migrate = Migrate()
+
+
 def create_app(config_name: str = 'production'):
 
     app = Flask(__name__, instance_relative_config=True)
-
     app.config.from_object(config[config_name])
+
+    from .import models
+
+    db.init_app(app)
+    migrate.init_app(app=app, db=db)
 
     return app
