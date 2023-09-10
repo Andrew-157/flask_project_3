25e45c53b999f2359e66150e3afbbfd394675f21
Andrew Subotin <subotinandrey5@gmail.com>
2023-09-10 09:52:15 +0200

    Private Page template route

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template
 
+from .. import login_required
 
 bp = Blueprint(name='main',
                import_name=__name__)
@@ -9,3 +10,9 @@ bp = Blueprint(name='main',
 @bp.route(rule='/', methods=['GET'])
 def index():
     return render_template('main/index.html')
+
+
+@bp.route('/you/', methods=['GET'])
+@login_required
+def private_page():
+    return render_template('main/private_page.html')
